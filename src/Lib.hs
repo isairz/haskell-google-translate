519dee454f05f78e7538561529d4cb49cfa35996
Seongjun Kim <isair.zun@gmail.com>
2016-12-06 01:20:38 +0900

    Parse TKK

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,7 +6,7 @@ import Token
 import Network.HTTP.Simple
 import qualified Data.ByteString.Lazy.Char8 as L8
 
-translate :: String -> IO (String)
+translate :: String -> IO String
 translate src = do
   token <- getToken
   return token
--- src/Token.hs
+++ src/Token.hs
@@ -2,15 +2,23 @@ module Token
     ( getToken
     ) where
 import           Network.HTTP.Simple
+import           Text.Regex.Posix
+import           Text.Regex.Posix.ByteString.Lazy
 import qualified Data.ByteString.Lazy.Char8 as L8
 
-updateTKK :: IO (L8.ByteString)
+-- FIXME: cache TKK per hours
+updateTKK :: IO (Integer, Integer)
 updateTKK = do
-  let request = "https://translate.google.com/"
-  response <- httpLBS request
-  return $ getResponseBody response
+  response <- httpLBS "https://translate.google.com/"
+  let body = getResponseBody response
+  let pat = "TKK=[^ ]* a\\\\x3d([0-9]+);var b\\\\x3d(-?[0-9]+);return ([0-9]+)" :: L8.ByteString
+  let (_,_,_,d) = (body =~ pat) :: (L8.ByteString, L8.ByteString, L8.ByteString, [L8.ByteString])
+  let a:b:c:_ = d
+  let time = read $ L8.unpack c
+  let key = (read $ L8.unpack a) + (read $ L8.unpack b)
+  return (time, key)
 
-getToken :: IO (String)
+getToken :: IO String
 getToken = do
     tkk <- updateTKK
     return $ show $ tkk
