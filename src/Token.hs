f2f8ec02515e1c8c03a44a3974c5c4a13fd7368d
Seongjun Kim <isair.zun@gmail.com>
2016-12-06 01:37:41 +0900

    let style to where style

--- src/Token.hs
+++ src/Token.hs
@@ -2,23 +2,25 @@ module Token
     ( getToken
     ) where
 import           Network.HTTP.Simple
-import           Text.Regex.Posix
+import           Text.Regex.Posix ((=~))
 import           Text.Regex.Posix.ByteString.Lazy
 import qualified Data.ByteString.Lazy.Char8 as L8
+
+getToken :: IO String
+getToken = do
+  tkk <- updateTKK
+  return $ show $ tkk
 
 -- FIXME: cache TKK per hours
 updateTKK :: IO (Integer, Integer)
 updateTKK = do
   response <- httpLBS "https://translate.google.com/"
-  let body = getResponseBody response
-  let pat = "TKK=[^ ]* a\\\\x3d([0-9]+);var b\\\\x3d(-?[0-9]+);return ([0-9]+)" :: L8.ByteString
-  let (_,_,_,d) = (body =~ pat) :: (L8.ByteString, L8.ByteString, L8.ByteString, [L8.ByteString])
-  let a:b:c:_ = d
-  let time = read $ L8.unpack c
-  let key = (read $ L8.unpack a) + (read $ L8.unpack b)
-  return (time, key)
+  return . parseTKK $ getResponseBody response
 
-getToken :: IO String
-getToken = do
-    tkk <- updateTKK
-    return $ show $ tkk
+parseTKK :: L8.ByteString -> (Integer, Integer)
+parseTKK body = (time, key)
+  where pat = "TKK=[^ ]* a\\\\x3d([0-9]+);var b\\\\x3d(-?[0-9]+);return ([0-9]+)" :: L8.ByteString
+        (_,_,_,(a:b:c:_)) = (body =~ pat) :: (L8.ByteString, L8.ByteString, L8.ByteString, [L8.ByteString])
+        time = read $ L8.unpack c
+        key = (read $ L8.unpack a) + (read $ L8.unpack b)
+
