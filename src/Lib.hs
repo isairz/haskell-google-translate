2ad5fa592ae77bb3d0b21342f9eb4524f54e4e27
Seongjun Kim <isair.zun@gmail.com>
2016-12-06 23:59:37 +0900

    Get Translated sentance

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,15 +5,17 @@ module Lib
 import Token
 import Data.Aeson (Value)
 import Network.HTTP.Simple
-import Data.ByteString.UTF8
-import qualified Data.ByteString.Lazy.UTF8 as L8
+import Text.Show.Unicode
 import qualified Data.ByteString.Char8 as B8
+import qualified Data.ByteString.UTF8 as B8
+import qualified Data.ByteString.Lazy.Char8 as L8
+import qualified Data.ByteString.Lazy.UTF8 as L8
 
 queryString :: String -> String -> String -> String -> [(B8.ByteString, Maybe B8.ByteString)]
 queryString sl tl src tk = [ ("client", Just "t")
-                           , ("sl", Just $ fromString sl)
-                           , ("tl", Just $ fromString tk)
-                           , ("hl", Just $ fromString tk)
+                           , ("sl", Just $ B8.fromString sl)
+                           , ("tl", Just $ B8.fromString tl)
+                           , ("hl", Just $ B8.fromString tl)
                            , ("dt", Just "at")
                            , ("dt", Just "bd")
                            , ("dt", Just "ex")
@@ -31,8 +33,8 @@ queryString sl tl src tk = [ ("client", Just "t")
                            , ("ssel", Just "3")
                            , ("tsel", Just "3")
                            , ("kc", Just "0")
-                           , ("tk", Just $ fromString tk)
-                           , ("q", Just $ fromString src)
+                           , ("tk", Just $ B8.fromString tk)
+                           , ("q", Just $ B8.fromString src)
                            ]
 -- "client=t&sl=en&tl=ja&hl=en&dt=at&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&ie=UTF-8&oe=UTF-8&otf=2&rom=1&ssel=0&tsel=0&kc=1&tk=335610.219646&q=sdfs"
 
@@ -40,6 +42,9 @@ translate :: String -> String -> String -> IO String
 translate sl tl src = do
   token <- getToken src
   response <- httpLBS $ setRequestQueryString (queryString sl tl src token)
-                       $ "https://translate.google.com/translate_a/single"
-  let temp = getResponseBody response
-  return $ show temp
+                      $ "https://translate.google.com/translate_a/single"
+  let raw = L8.toString $ getResponseBody response -- JSON was broken...
+  return $ takeWhile (/= '\"')
+         $ drop 1
+         $ dropWhile (/='\"')
+         $ raw
