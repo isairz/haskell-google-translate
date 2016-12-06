a38fb7da820d61018f0bced9b366135f2c2d8cbd
Seongjun Kim <isair.zun@gmail.com>
2016-12-06 21:00:39 +0900

    implement tk

--- src/Token.hs
+++ src/Token.hs
@@ -3,6 +3,7 @@ module Token
     ) where
 
 import           Data.Bits
+import           Data.Char
 import           Network.HTTP.Simple
 import           Text.Regex.Posix ((=~))
 import           Text.Regex.Posix.ByteString.Lazy
@@ -11,8 +12,7 @@ import qualified Data.ByteString.Lazy.Char8 as L8
 
 getToken :: String -> IO String
 getToken src = do
-  -- tkk <- updateTKK
-  let tkk = (0,0)
+  tkk <- updateTKK
   return $ generateTk tkk src
 
 -- FIXME: cache TKK per hours
@@ -33,10 +33,13 @@ generateTk (time, key) src = do
   let utf8 = map toInteger $ encode src
   let r1 = foldl (\r c -> xr (r + c) "+-a^+6") time utf8
   let r2 = xor (xr r1 "+-3^+b+-f") key;
-  let r3 = if r2 < 0 then (r2 .&. 2147483647) + 2147483648 else r2
-  let r4 = mod r3 1000000
-  (show r4) ++ "." ++ (show $ xor r4 time)
+  let r3 = mod r2 1000000
+  (show r3) ++ "." ++ (show $ xor r3 time)
 
--- TODO: implement
-xr :: Integer -> String -> Integer
-xr a b = a
+xr :: Integer -> [Char] -> Integer
+xr r (a:b:c:rest) = xr r' rest
+  where r1 = digitToInt c
+        r2 = (if b == '+' then shiftR else shiftL) r r1
+        r3 = (if a == '+' then (+) else xor) r r2
+        r' = r3 .&. 0xffffffff
+xr r _ = r
